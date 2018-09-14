1ed85b646b1697f13c4ba2e76e607f7d203a271f
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-14 22:26:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-function printBadges ([nameArray]) {
+function printBadges (nameArray) {
   for (let i = 0; i < nameArray.length; i++) {
     console.log("Welcome, ${nameArray[i]}! You are employee #${i+1}");
   }
